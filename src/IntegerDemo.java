6dcd8cf6b46c9ba358bdb928a17f826cf5a7dac2
Esteban Revelo <estebanadso21@gmail.com>
2025-05-02 21:52:14 -0500

    operaciones aritméticas básicas

--- src/IntegerDemo.java
+++ src/IntegerDemo.java
@@ -1,7 +1,28 @@
-public class IntegerDemo {
+/*public class IntegerDemo {
     public static void main(String[] args) {
 			// TODO: Declare an integer variable and print it
             int number = 42;
             System.out.println("The number is: " + number);
     }
+}*/
+
+public class IntegerDemo {
+    public static void main(String[] args) {
+        int number1 = 10;
+        int number2 = 5;
+
+			// TODO: Perform arithmetic operations and print the results
+        int sum = number1 + number2;
+        int difference = number1 - number2;
+        int product = number1 * number2;
+        int quotient = number1 / number2;
+        int remainder = number1 % number2;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+        System.out.println("Remainder: " + remainder);
+    }
 }
+
