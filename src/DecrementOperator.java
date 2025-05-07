b5b88fcccd7f2d037aea0ae966ee7a23946105c4
Esteban Revelo <estebanadso21@gmail.com>
2025-05-06 19:13:01 -0500

    Operador de Decremento( --)

---
+++ src/DecrementOperator.java
@@ -0,0 +1,14 @@
+public class DecrementOperator {
+    public static void main(String[] args) {
+        int nivel = 3;
+
+        // TODO: Aplicar decremento
+        System.out.println("Valor inicial: " + nivel);
+
+        nivel--;
+        System.out.println("Después de nivel--: " + nivel);
+            
+        --nivel;
+        System.out.println("Después de --nivel: " + nivel);
+    }
+}
