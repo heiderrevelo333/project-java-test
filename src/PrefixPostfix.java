502fc32e524868a6b8f873885d8e90946a9199c6
Esteban Revelo <estebanadso21@gmail.com>
2025-05-06 19:15:36 -0500

    Prefijo vs Posfijo

---
+++ src/PrefixPostfix.java
@@ -0,0 +1,13 @@
+public class PrefixPostfix {
+    public static void main(String[] args) {
+        // TODO: Demostrar diferencia entre prefijo y posfijo
+        int a = 5;
+        int b = ++a; // Prefijo: primero incrementa, luego asigna
+        System.out.println("a: " + a + ", b (prefijo): " + b);
+            
+        int x = 5;
+        int y = x++; // Posfijo: primero asigna, luego incrementa
+        System.out.println("x: " + x + ", y (posfijo): " + y);
+
+    }
+}
